beb155ceb20ca1fd310c1fb5885abea3293b452d
Md Abbas Ali <mdabbasali185@gmail.com>
2022-06-08 13:09:45 +0600

    component complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home';
-import Card from './Pages/Card/Card';
-import Header from './Pages/Shared/Header';
-import Contact from './Pages/Contact/Contact';
+import Header from "../src/Pages/Shared/Header";
+import Intro from "../src/Pages/Intro/Intro.jsx";
+import Services from "../src/Pages/Services/Services.jsx";
+import Experience from "../src/Pages/Experience/Experience.jsx";
+import Works from "../src/Pages/Works/Works.jsx";
+import Portfolio from "../src/Pages/Portfolio/Portfolio.jsx";
+import Testimonial from "../src/Pages/Testimonials/Testimonial.jsx";
+import Contact from "../src/Pages/Contact/Contact";
+import Footer from "../src/Pages/Shared/Footer/Footer.jsx";
+import { useContext } from "react";
+import { themeContext } from "./Context";
+import "./App.css";
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
+    <div className='App'
+
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+
+
+
       <Header />
-
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/card' element={<Card />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
-
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }

dbbde1daf60296092e4f2447c37d379433cd73a6
Md Abbas Ali <mdabbasali185@gmail.com>
2022-06-08 11:03:02 +0600

    first commit

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './Shared.css';
 
-import './Shared.css'
 
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false)
 
     useEffect(() => {
         const navLink = document.getElementsByClassName('nav-link')
@@ -27,48 +29,66 @@ const Header = () => {
             hoverBg.style.left = x + 'px'
             hoverBg.style.width = active.clientWidth + 'px'
         })
+
         const active = document.querySelector('.active')
         active.style.color = 'white'
         hoverBg.style.width = active.clientWidth + 'px'
 
+
     }, [])
 
-
+   
     return (
-        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <Link class="navbar-brand" to="/">UN</Link>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-capitalize">
-                        <li class="nav-item">
-                            <NavLink class="active" aria-current="page" to="/">Home</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/about">about </NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="#contact">contact</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="#work">work</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="#work">work</NavLink>
-                        </li>
-                        <span id="hoverBg" ></span>
-                    </ul>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light position-relative">
+            <div className="container">
+                <Link className="navbar-brand" to="/">UN</Link>
+
+
+                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-none d-lg-flex text-capitalize">
+                    <li className="nav-item">
+                        <NavLink className="active" aria-current="page" to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about">about </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#contact">contact</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#work">work</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#work">work</NavLink>
+                    </li>
+                    <span id="hoverBg" ></span>
+                </ul>
+                <div className={`toggleButton d-block d-lg-none  ms-auto ${toggle && 'active'}`} onClick={() => setToggle(!toggle)} >
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
+
+                <ul className={`myNav text-capitalize ${toggle && 'active'} `}>
+                    <li className="nav-item">
+                        <NavLink className="active" aria-current="page" to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about">about </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#contact">contact</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#work">work</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="#work">work</NavLink>
+                    </li>
+                    <span id="hoverBg" ></span>
+                </ul>
+
+
+
             </div>
         </nav>
 

bd28152038d033fa6f7fcd362fa65cefe22c6975
Md Abbas Ali <mdabbasali185@gmail.com>
2022-06-08 11:36:23 +0600

    header edit

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -12,83 +12,74 @@ const Header = () => {
         const navbar_nav = document.querySelector('.navbar-nav')
         const hoverBg = document.getElementById('hoverBg')
 
+        for (let i = 0; i < navLink.length; i++) {
+            const element = navLink[i];
+            element.addEventListener('click', () => {
+                const active = document.querySelector('.active')
+                active.classList.remove('active')
+                element.classList.add('active')
+
+
+            })
+
+        }
+
+
         navbar_nav.addEventListener('mousemove', (e) => {
             const active = document.querySelector('.active')
-            active.style.color = '#333'
+            active.style.color = 'black'
             e.target.style.color = 'white'
             const x = e.target.offsetLeft
             hoverBg.style.left = x + 'px'
             hoverBg.style.width = e.target.clientWidth + 'px'
 
         })
+
         navbar_nav.addEventListener('mouseout', (e) => {
             const active = document.querySelector('.active')
-            e.target.style.color = '#333'
+            e.target.style.color = 'black'
             active.style.color = 'white'
             const x = active.offsetLeft
             hoverBg.style.left = x + 'px'
             hoverBg.style.width = active.clientWidth + 'px'
         })
-
         const active = document.querySelector('.active')
         active.style.color = 'white'
-        hoverBg.style.width = active.clientWidth + 'px'
-
-
     }, [])
 
-   
+
     return (
-        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light position-relative">
-            <div className="container">
-                <Link className="navbar-brand" to="/">UN</Link>
-
-
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0 d-none d-lg-flex text-capitalize">
-                    <li className="nav-item">
-                        <NavLink className="active" aria-current="page" to="/">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/about">about </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#contact">contact</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#work">work</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#work">work</NavLink>
-                    </li>
-                    <span id="hoverBg" ></span>
-                </ul>
-                <div className={`toggleButton d-block d-lg-none  ms-auto ${toggle && 'active'}`} onClick={() => setToggle(!toggle)} >
-                    <span></span>
-                    <span></span>
-                    <span></span>
+        <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">UN</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-capitalize">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">about </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#work">work</a>
+                        </li>
+                        <span id="hoverBg" ></span>
+                    </ul>
                 </div>
-
-                <ul className={`myNav text-capitalize ${toggle && 'active'} `}>
-                    <li className="nav-item">
-                        <NavLink className="active" aria-current="page" to="/">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/about">about </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#contact">contact</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#work">work</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="#work">work</NavLink>
-                    </li>
-                    <span id="hoverBg" ></span>
-                </ul>
-
-
-
             </div>
         </nav>
 
